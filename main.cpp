00cea42915a74c1c7e3c5d73ffd097d320deecc5
rubydave87 <rubydave11@gmail.com>
2024-05-17 10:23:25 +0000

    coolterm not working correct
    Signed-off-by: rubydave87 <rubydave11@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 
 InterruptIn butt(PC_13);
 
+volatile int myflag = 0;
+
 void buttn_fall(){
-printf("button pressed!!\r\n");
+myflag = 1;
 }
 
 //main() runs in its own thread in the OS
@@ -12,9 +14,11 @@ int main()
 {
     printf("This is mbed os %d \r\n" , MBED_VERSION);
     butt.fall(&buttn_fall);
-
 while (true) {
-
-    }
+if (myflag == 1){
+    printf("button pressed!!\r\n");
+    myflag = 0;
 }
-
+    
+}
+}
